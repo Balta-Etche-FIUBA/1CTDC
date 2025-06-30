58058d6ad612bb3a66d5fc921f363398d65843e9
Gonzalo Antahuara <gantahuara@fi.uba.ar>
2025-06-30 00:19:26 -0300

    Last RR

--- CP5_buck_pwm/Disipador.m
+++ CP5_buck_pwm/Disipador.m
@@ -1,28 +1,28 @@
 
 %%
 
-fsw = 120e3;       % Frecuencia de conmutación [Hz]
-Vdrv = 11;         % Voltaje del driver [V]
-Cgate = 43e-9;      % Capacidad de gate [F]
-% Qgate = Cgate * Vdrv;  % Carga de gate [C] (opcional)
-Vds = 30;         % Tensión drenaje-fuente [V]
-Id = 1.5;           % Corriente del drain [A]
-Rds_on = 17.5e-3;     % R_DS(on) del transistor [Ohm]
-D = 0.7;           % Ciclo de trabajo (duty cycle)
-tsw = 60e-9 + 45e-9;      % Tiempo de conmutación [s]
+fsw = 120e3;       
+Vdrv = 11;         
+Qgate = 43e-9;     
+% Qgate = Cgate * Vdrv;  
+Vds = 30;         
+Id = 1.5;           
+Rds_on = 17.5e-3;     
+D = 0.316;           
+tsw = 60e-9 + 45e-9;     
 
-% Cálculos
-Pgate = fsw * Cgate * Vdrv^2
+
+Pgate = fsw * Qgate * Vdrv
 Pcond = Id^2 * Rds_on * D
 Psw = 0.5 * Vds * Id * fsw * tsw 
 
 Ploss = Psw + Pcond + Pgate
 
-%%Disipador
+
 
 Tj = 125;       
 Ta = 25;        
-Pc_max = Ploss;    
+Pc_max = Pgate + Psw;    
 
 theta_jc = 1.5; 
 theta_cs = 0.5; 
--- CP5_buck_pwm/cboost.m
+++ CP5_buck_pwm/cboost.m
@@ -1,8 +1,8 @@
 
-Qgate = 14e-9;       
+Qgate = 63e-9;       
 Qrr = 60e-12;   
 % Qrr = 1e-9;
-Iboot = 200e-3;        
+Iboot = 0.5e-3;        
 fsw = 120e3;         
 dVboot = 0.1;        
 
